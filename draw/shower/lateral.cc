efa46ef2c3b83f13c7000d6e9b0835ff8dd7757b
Ho-San KO <gboon18@gmail.com>
2019-03-23 07:35:44 -0400

    Updated; finding maxima energy deposited crystal. Tree name changed; t2->t_Flux

--- draw/shower/lateral.cc
+++ draw/shower/lateral.cc
@@ -3,10 +3,10 @@
   double Eb = 1e4;//Beam energy
   double percent = 100;//change the unit to %
 
-  double totedep = 0.0;
+  double totedep = 0.0;// MeV
 
   TFile *f = new TFile("test.root");
-  TTree *t = f->Get("t2");
+  TTree *t = f->Get("t_Flux");
 
   //call trees "and then" open write file
   TFile *f_write = new TFile("output/lateral.root", "RECREATE");
@@ -30,16 +30,19 @@
   double y[array];
   double z[array];
 
+  double edep_store[array] = {0.};
+
   int    evtNb_store = -1;
-  int    evtNb_notgood = -1;
-  int    evtNb_goodcount = 0;
-  int    evtNb_totcount = 0;
-  int    j_store = -1;
+  vector<int>    evtNb_goodcount;
   int    x_store = -1;
   int    y_store = -1;
 
   double x_pos_store = -1.;
   double y_pos_store = -1.;
+
+  double highestEdep = 0.;
+  int    highestEdep_x = -1;
+  int    highestEdep_y = -1;
 
   t->SetBranchAddress("evtNb", &evtNb);
   t->SetBranchAddress("edep", &edep);
@@ -47,90 +50,86 @@
   t->SetBranchAddress("y", &y);
   t->SetBranchAddress("z", &z);
 
-  TH1D *h = new TH1D("h", "Lateral energy profile", bin, 0, R);
+  TH1D *h = new TH1D("h", "", bin, 0, R);
 
-  TH2D *h2D_1 = new TH2D("h2D_1", "Pattern on the calorimeter", 30, 0, 30, 36, 0, 36);//in x axis
-  TH2D *h2D_2 = new TH2D("h2D_2", "Pattern on the calorimeter except hits on the edge", 30, 0, 30, 36, 0, 36);//in x axis
+  TH2D *h2D_1 = new TH2D("h2D_1", "", 30, 0, 30, 36, 0, 36);
+  TH2D *h2D_2 = new TH2D("h2D_2", "", 30, 0, 30, 36, 0, 36);
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////////////////////////
 
   for(int i = 0 ; i < t->GetEntries() ; i++){
-    t->GetEntry(i);
+    t->GetEntry(i); if(i%20000 == 0)cout<<""<<(double)(1.0*i /t->GetEntries())*100<<"%"<<endl;
+    if(evtNb!=evtNb_store) {memset(edep_store, 0., sizeof(edep_store)); evtNb_store = evtNb;}
     for(int j = 0 ; j < array ; j++){
       x = int(j/36);
       y = j%36;
-      if(edep[j]){if(i%20000 == 0) cout<<"totEdep calculating : "<<(double)(1.0*i/t->GetEntries())*100<<"%"<<endl;
-	if(evtNb!=evtNb_store){ 
-	  evtNb_totcount++;
-	  if ((x!=0 && x!=1 && x!=28 && x!=29) && (y!=0 && y!=1 && y!=34 && y!=35)){
-	    evtNb_goodcount++;
-	  }
-	  else{
-	    evtNb_notgood = evtNb;
-	  }
-	}
-      }
+      edep_store[j] += edep[j];
+      h2D_1->Fill(x, y, edep[j]); 
     }
-    evtNb_store = evtNb;
+
+    t->GetEntry(i+1);
+    if(evtNb!=evtNb_store){
+      highestEdep = 0.;
+      highestEdep_x = -1;
+      highestEdep_y = -1;
+      for(int k = 0 ; k < array ; k++){
+  	x = int(k/36);
+  	y = k%36;
+  	if(edep_store[k]>highestEdep){
+  	  highestEdep = edep_store[k];
+  	  highestEdep_x = x;
+  	  highestEdep_y = y;
+  	}
+      }
+      if((highestEdep_x!=0 && highestEdep_x!=1 && highestEdep_x!=28 && highestEdep_x!=29) 
+  	 && (highestEdep_y!=0 && highestEdep_y!=1 && highestEdep_y!=34 && highestEdep_y!=35)
+  	 && (highestEdep_x!=-1 && highestEdep_y!=-1)){
+  	evtNb_goodcount.push_back(evtNb_store);
+      }   
+    }
   }
-
-  totedep = evtNb_goodcount*Eb;
-  cout<<"Total counts  : "<<evtNb_totcount<<endl;
-  cout<<"Good  counts  : "<<evtNb_goodcount<<endl;
-  cout<<"totedep : "<<totedep<<" MeV"<<endl;
-
+  totedep = evtNb_goodcount.size()*Eb;
   /////////////////////
   evtNb_store   = -1;//
-  evtNb_notgood = -1;//
   /////////////////////
-
   for(int i = 0 ; i < t->GetEntries() ; i++){
-    t->GetEntry(i);
-    for(int j = 0 ; j < array ; j++){
-      x = int(j/36);
-      y = j%36;
-      if(edep[j]){if(i%20000 == 0) cout<<""<<(double)(1.0*i/t->GetEntries())*100<<"%"<<endl;
-	if(evtNb!=evtNb_store){
-	  if((x!=0 && x!=1 && x!=28 && x!=29) && (y!=0 && y!=1 && y!=34 && y!=35)){
-	    j_store = j;
-	    x_store = x;
-	    y_store = y;
+    t->GetEntry(i); if(i%20000 == 0)cout<<"Filling: "<<(double)(1.0*i/t->GetEntries())*100<<"%"<<endl;
+    //    if(find(evtNb_goodcount.begin(), evtNb_goodcount.end(), evtNb)!=evtNb_goodcount.end()){}// does not work in ROOT v5
+    for(int k = 0 ; k < evtNb_goodcount.size() ; k++){
+      if(evtNb == evtNb_goodcount[k]){
+  	for(int j = 0 ; j < array ; j++){
+  	  x = int(j/36);
+  	  y = j%36;
+  	  h2D_2->Fill(x, y, edep[j]);
+  	  if(edep[j] && evtNb!=evtNb_store){
 
-	    x_pos_store = x[j];
-	    y_pos_store = y[j];
+  	    evtNb_store = evtNb;
 
-	    h->Fill(0, edep[j]/totedep*percent);
-	    h2D_2->Fill(x, y, edep[j]); 
-	  }
-	  else{
-	    evtNb_notgood = evtNb;
-	  }
-	}
-	else if(evtNb != evtNb_notgood){ 
-	  h->Fill(sqrt( (x_pos_store + (-x[j] + (x - x_store)*X))*(x_pos_store + (-x[j] + (x - x_store)*X)) + (y_pos_store - (y[j] + (y - y_store)*Y))*(y_pos_store - (y[j] + (y - y_store)*Y)) ), edep[j]/totedep*percent );
+  	    x_store = x;
+  	    y_store = y;
 
-	  h2D_2->Fill(x, y, edep[j]); 
-	}
+  	    x_pos_store = x[j];
+  	    y_pos_store = y[j];
 
-	h2D_1->Fill(x, y, edep[j]); 
+  	    h->Fill(0, edep[j]/totedep*percent);
+  	  }
+  	  else{
+  	    h->Fill(sqrt( (x_pos_store + (-x[j] + (x - x_store)*X))*(x_pos_store + (-x[j] + (x - x_store)*X)) + (y_pos_store - (y[j] + (y - y_store)*Y))*(y_pos_store - (y[j] + (y - y_store)*Y)) ), edep[j]/totedep*percent );
+  	  }
+  	}
       }
     }
-    evtNb_store = evtNb;
   }
 
-  cout<<"Total counts  : "<<evtNb_totcount<<endl;
-  cout<<"Good  counts  : "<<evtNb_goodcount<<endl;
   cout<<"totedep : "<<totedep<<" MeV"<<endl;
 
-  cout<<"totedep     : "<<totedep<<" MeV"<<endl;
-
   ////////////////////////////////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  TCanvas *c_pattern = new TCanvas("c_pattern", "Pattern on the calorimeter", 200, 10, 300*2, 360*2);
-  TCanvas *c_pattern_2 = new TCanvas("c_pattern_2", "Pattern on the calorimeter except hits on the edge", 200, 10, 300*2, 360*2);
-  TCanvas *c_edep    = new TCanvas("c_edep",    "Lateral energy profile", 200, 10, 300*2, 360*2);
+  TCanvas *c_pattern = new TCanvas("c_pattern", "1", 200, 10, 300*2, 360*2);
+  TCanvas *c_pattern_2 = new TCanvas("c_pattern_2", "2", 200, 10, 300*2, 360*2);
+  TCanvas *c_edep    = new TCanvas("c_edep",    "", 200, 10, 300*2, 360*2);
 
   c_pattern->cd();
   h2D_1->Draw("colz");
@@ -143,7 +142,6 @@
   h->GetXaxis()->SetTitle("R [mm]");
   h->GetYaxis()->SetTitle("#frac{1}{E}#frac{dE}{dR} [%]");
   h->GetXaxis()->SetRangeUser(0, 100);
-  //  h->GetYaxis()->SetRangeUser(1, 100);
   h->SetLineColor(1);
   h->SetMarkerColor(1);
   h->Draw();
@@ -153,5 +151,6 @@
   leg->Draw();
 
   f_write->Write();
-  // f_write->Close();
+  //  f_write->Close();
+  evtNb_goodcount.clear();
 }
--- draw/shower/longitudinal_integral.cc
+++ draw/shower/longitudinal_integral.cc
@@ -1,41 +1,35 @@
 {
   TFile *f = new TFile("output/longitudinal.root", "READ");
-  TCanvas *c1 = new TCanvas("c1", "", 200, 10, 700, 500);
+  TCanvas *c1 = new TCanvas("c1", "Longitudinal energy profile", 200, 10, 700, 500);
   gPad->SetGridx(1);
   gPad->SetGridy(1);
-  h_10GeV->GetXaxis()->SetTitle("X_{0}");
-  h_10GeV->Draw();
+  gPad->SetLogy(1);
+  h->Draw();
 
   TLegend *leg = new TLegend(.6, .6, .9, .9 );
-  leg->AddEntry(h_10GeV, "10GeV", "l");
+  leg->AddEntry(h, "Longitudinal profile", "l");
   leg->Draw();
 
-  c1->Print("output/output/longitudinal.pdf");
-  gPad->SetLogy(1);
   c1->Print("output/output/longitudinal_logy.pdf");
   ////////////////////////////////////////////////////////////////////
 
-  TH1* hc_10GeV = h_10GeV->GetCumulative();
+  TH1* hc = h->GetCumulative();
 
-  hc_10GeV->Scale(100/h_10GeV->Integral());
+  hc->Scale(100/h->Integral());
 
-  TCanvas *c2 = new TCanvas("c2", "", 200, 10, 700, 500);
+  TCanvas *c2 = new TCanvas("c2", "Total longitudinal energy deposition", 200, 10, 700, 500);
   gPad->SetGridx(1);
   gPad->SetGridy(1);
-  gPad->SetLogy(1);
-  hc_10GeV->SetTitle("Cumulated energy deposition in Calorimeter");
-  hc_10GeV->GetXaxis()->SetTitle("X_{0}");
-  hc_10GeV->GetYaxis()->SetTitle("Energy deposition [%]");
-  hc_10GeV->Draw();
+  gPad->SetLogy(0);
+  hc->SetTitle("Total longitudinal energy deposition in Calorimeter");
+  hc->GetXaxis()->SetTitle("X_{0}");
+  hc->GetYaxis()->SetTitle("Energy deposition [%]");
+  hc->Draw();
 
   TLegend *leg_c = new TLegend(0.6, 0.1, 0.9, 0.4 );
-  leg_c->AddEntry(hc_10GeV, "10GeV", "l");
+  leg_c->AddEntry(hc, "Total longitudinal energy deposition", "l");
   leg_c->Draw();
 
-  c2->Print("output/output/longitudinal_integral_logy.pdf");
-  c2->cd();
-  gPad->SetLogy(0);
-  c2->Update();
   c2->Print("output/output/longitudinal_integral.pdf");
 
 }
